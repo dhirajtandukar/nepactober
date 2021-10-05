cc4512328ec2788e0f552a979b98564703305a1e
Prativa tandukar <prativatand@gmail.com>
2021-10-05 16:16:15 +0545

    yo nigga

---
+++ policy/index.php
@@ -0,0 +1,93 @@
+<?php
+include('include/header.php');
+?>
+
+
+
+<!-- Practice Section2 -->
+<section class="practice-section2">
+  <h3>Our Legal Practice Areas</h3>
+  <div class="row">
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon ">
+          <img src="assets/img/icons/briefcase.svg" class="rotate"></div>
+        <h6>Foreign Direct Investment</h6>
+        <p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon">
+          <img src="assets/img/icons/car.svg" class="rotate"></div>
+        <h6>Energy and Infrastructure</h6>
+
+        <p></p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon">
+          <img src="assets/img/icons/car.svg" class="rotate"></div>
+        <h6>Comapny Law</h6>
+        <p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon">
+          <img src="assets/img/icons/icon-1.svg" class="rotate"></div>
+        <h6>Commercial Contracts</h6>
+        <p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon">
+          <img src="assets/img/icons/icon-3.svg" class="rotate"></div>
+        <h6>Bank and Finance</h6>
+        <p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon">
+          <img src="assets/img/icons/icon-3.svg" class="rotate"></div>
+        <h6>Intellectual Property</h6>
+        <p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon">
+          <img src="assets/img/icons/icon-7.svg" class="rotate"></div>
+        <h6>Labour Law</h6>
+        <p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="p-box">
+        <div class="icon">
+          <img src="assets/img/icons/icon-1.svg" class="rotate"></div>
+        <h6>Immigration</h6>
+        <p>knsadlksdjlkdsa ksadnlaskd sadknaskdna asndk</p>
+        <a class="arrow" href="foreign.php"><img src="assets/img/icons/right.svg"></a>
+      </div>
+    </div>
+  </div>
+  <button class="btn-3">View more <i class="fas fa-arrow-right"></i></button>
+</section>
+<!-- End of Practice Section2 -->
+
+<?php
+include('include/footer.php');
+?>
